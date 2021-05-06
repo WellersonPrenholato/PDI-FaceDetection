085daf7a130df76864927b98a7e9b9e085aeb982
Wellerson Prenholato <wellerson.prenholato@gmail.com>
2021-05-06 19:21:10 -0300

    Update Greenness.java

--- PDI_1_0/nbproject/br/implementacoes/Greenness.java
+++ PDI_1_0/nbproject/br/implementacoes/Greenness.java
@@ -24,12 +24,19 @@ public class Greenness {
 	 * @return retorna a imagem depois de aplicado o filtro
 	 */
 	public BufferedImage GreennKG(BufferedImage img) {
-		BufferedImage res = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+		BufferedImage res = new BufferedImage(img.getWidth(), img.getHeight(), img.getType()); //Carregamento da imagem para res
 
+		// Declaração de variáveis
 		float R, G, B;
 		float maxS =0, minS=1;	
 		float maxT=0, minT=1;
-		
+
+		//Hue -> Matiz
+		//Saturation -> Saturação
+		//Brightness -> Brilho
+
+		// Essa estrutura percorre toda a imagem, levando em consideração sua altura e largura.
+		// O intuito é encontrar o mínimo e máximo de S(Saturation) e T(Matiz), dentro da imagem.
 		for (int i = 0; i < img.getWidth(); i++) {
 			for (int j = 0; j < img.getHeight(); j++) {
 				Color cor = new Color(img.getRGB(i, j));
@@ -38,47 +45,47 @@ public class Greenness {
 				G = cor.getGreen();
 				B = cor.getBlue();
 				
-				// System.out.println("\n1 - Red:" + R + " Verde:" + G + " Azul:" + B);
-	
-				float[] hsbValues = Color.RGBtoHSB((int)R, (int)G, (int)B, null);
+				float[] hsbValues = Color.RGBtoHSB((int)R, (int)G, (int)B, null);  // Conversão de RGB para HSB (Hue, Saturation, Brightness)
 				float hue = hsbValues[0]; 
 				float saturation = hsbValues[1];
 				// float brightness = hsbValues[2];
-
+				
+				// Condicional utilizado para encontrar o valor máximo da Saturação na imagem
 				if (maxS >= saturation){
 					maxS = saturation;
 				}
 
+				// Condicional utilizado para encontrar o valor mínimo da Saturação na imagem
 				if (minS <= saturation){
 					minS = saturation;
 				}
 
+				// Condicional utilizado para encontrar o valor máximo da Matiz na imagem
 				if (maxT >= hue){
 					maxT = hue;
 				}
 
+				// Condicional utilizado para encontrar o valor máximo da Matiz na imagem
 				if (minT <= hue){
 					minT = hue;
 				}
 			}
 		}
 
-		for (int i = 0; i < img.getWidth(); i++) {
-			for (int j = 0; j < img.getHeight(); j++) {
-				Color cor = new Color(img.getRGB(i, j));
-				// Color spaceColor = new Color(img.getColorSpace(i,j));
-				R = cor.getRed();
-				G = cor.getGreen();
-				B = cor.getBlue();
-				
-				float[] hsbValues = Color.RGBtoHSB((int)R, (int)G, (int)B, null);
-				float hue = hsbValues[0]; 
-				float saturation = hsbValues[1];
-				float brightness = hsbValues[2];
-				
-				// System.out.println("\n Hue: " + hue + " Saturacao: " + saturation + " Brilho: "+ brightness);
 
-				float S = (saturation - minS)/(maxS - minS);
+		for (int i = 0; i < img.getWidth(); i++) { 
+			for (int j = 0; j < img.getHeight(); j++) {
+				Color cor = new Color(img.getRGB(i, j)); //Identifica a cor na posição ij
+				R = cor.getRed(); // Identifica o valor Red do pixel podendo ser 0-255, e atribuí esse valor a uma variável R.
+				G = cor.getGreen(); // Identifica o valor Green do pixel podendo ser 0-255, e atribuí esse valor a uma variável G.
+				B = cor.getBlue(); // Identifica o valor Blue do pixel podendo ser 0-255, e atribuí esse valor a uma variável B.
+
+				float[] hsbValues = Color.RGBtoHSB((int)R, (int)G, (int)B, null); // Conversão de RGB para HSB (Hue, Saturation, Brightness)
+				float hue = hsbValues[0]; // Atribuição do Hue, baseado na conversão da linha anterior.
+				float saturation = hsbValues[1]; // Atribuição da Saturation, baseado na conversão da linha anterior.
+				float brightness = hsbValues[2]; // Atribuição da Brightness, baseado na conversão da linha anterior.
+				
+				float S = (saturation - minS)/(maxS - minS); 
 				float T = (hue - minT)/(maxT - minT);
 
 				int hsbToRgb = Color.HSBtoRGB(T, S, brightness);
@@ -96,6 +103,13 @@ public class Greenness {
 			}
 		}
 
+		/* Em qualquer imagem com fundo complexo, a área da pele costuma ser menor que
+		os pixels não skin. Uma vez que usamos um procedimento de avaliação de pixels, para reduzir o
+		esforço de computação, aplicamos um filtro inicial que pode remover todos os pixels facilmente
+		rotulado como sem pele. Este filtro visa apenas remover pixels de processamento posterior e, 
+		com sorte, os pixels da capa não são removidos. Verde, azul, amarelo e outros bem
+		cores definidas são eliminadas usando regras empíricas definido no condicional nas linhas 118 à 126.
+		*/
 		for (int i = 0; i < res.getWidth(); i++) {
 			for (int j = 0; j < res.getHeight(); j++) {
 				Color cor = new Color(res.getRGB(i, j));
@@ -103,7 +117,7 @@ public class Greenness {
 				G = cor.getGreen();
 				B = cor.getBlue();
 				
-				Color pixelBranco = new Color(255, 255, 255);
+				Color pixelBranco = new Color(255, 255, 255); // Criação de uma variável referente ao pixel totalmente branco.
 
 				if ( (B > 160 && R < 180 && G < 180) || // Too much blue
 					(G > 160 && R < 180 && B < 180) || // Too much green
@@ -114,9 +128,9 @@ public class Greenness {
 					(B/(R+G+B) > .40) || // Too much blue in contrast to others
 					(G/(R+G+B) > .40) || // Too much green in contrast to others
 					(R < 102 && G > 100 && B > 110 && G < 140 && B < 160)){ // Ocean
-						res.setRGB(i, j, pixelBranco.getRGB());
+						res.setRGB(i, j, pixelBranco.getRGB()); // Caso o pixel não seja considerado pele, ele se tornará um pixel branco
 					}else{
-						res.setRGB(i, j, img.getRGB(i, j));
+						res.setRGB(i, j, img.getRGB(i, j)); // Caso o pixel seja considerado pele, o pixel da imagem é mantido.
 					}
 			}
 		}
